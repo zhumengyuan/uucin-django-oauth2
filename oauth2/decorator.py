be15ed0b0752ca50a6d9ba46f28236f7fc056873
liulinlin <liulinlin@zhumengyuan.com>
2015-07-31 14:30:51 +0800

    add json_stream_handle

--- oauth2/decorator.py
+++ oauth2/decorator.py
@@ -2,14 +2,14 @@
 
 
 import re
-from django.http.response import HttpResponseForbidden
+import json
+from django.http.response import HttpResponseForbidden, HttpResponseBadRequest
 from oauth2.passport import get_token_userid
 
 
 def token_required(view, required=True):
     def decorator(request, *args, **kwargs):
         request.access_token = None
-        request.uucode = None
         try:
             request.access_token = re.match(
                 '^token (\w+)', request.META['HTTP_AUTHORIZATION']).groups()[0]
@@ -18,5 +18,19 @@ def token_required(view, required=True):
         request.uucode = get_token_userid(request.access_token)
         if required:
             return HttpResponseForbidden()
+        return view(request, *args, **kwargs)
+    return decorator
+
+
+def json_stream_handle(view):
+    def decorator(request, *args, **kwargs):
+        if request.method in ['PUT', 'POST', 'PATCH']:
+            stream = request.body
+            if stream:
+                try:
+                    request.jsondata = json.loads(stream)
+                except ValueError:
+                    return HttpResponseBadRequest(json.dumps({
+                        "message": "Problems parsing JSON"}))
         return view(request, *args, **kwargs)
     return decorator

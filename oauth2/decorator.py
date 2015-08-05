fa8f146ed1c50efc810e2c2776b31f6b914c8ce3
yangxintong <yangxintong@zhumengyuan.com>
2015-08-05 09:45:16 +0800

    整合鉴权与流处理方法

--- oauth2/decorator.py
+++ oauth2/decorator.py
@@ -7,7 +7,7 @@ from django.http.response import HttpResponseForbidden, HttpResponseBadRequest
 from oauth2.passport import get_token_userid
 
 
-def token_required(view, required=True):
+def token_required(view, json_stream=True,required=True):
     def decorator(request, *args, **kwargs):
         request.access_token = None
         try:
@@ -18,19 +18,14 @@ def token_required(view, required=True):
         request.uucode = get_token_userid(request.access_token)
         if required and not request.uucode:
             return HttpResponseForbidden()
-        return view(request, *args, **kwargs)
-    return decorator
-
-
-def json_stream_handle(view):
-    def decorator(request, *args, **kwargs):
-        if request.method in ['PUT', 'POST', 'PATCH']:
-            stream = request.body
-            if stream:
-                try:
-                    request.jsondata = json.loads(stream)
-                except ValueError:
-                    return HttpResponseBadRequest(json.dumps({
-                        "message": "Problems parsing JSON"}))
+        if json_stream:
+            if request.method in ['PUT', 'POST', 'PATCH']:
+                stream = request.body
+                if stream:
+                    try:
+                        request.jsondata = json.loads(stream)
+                    except ValueError:
+                        return HttpResponseBadRequest(json.dumps({
+                            "message": "Problems parsing JSON"}))
         return view(request, *args, **kwargs)
     return decorator

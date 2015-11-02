68d173966184e8cbea5fc6964e8a5ce77342ccb1
yxteagle <yxteagle@163.com>
2015-11-02 13:12:17 +0800

    Update passport.py

--- oauth2/passport.py
+++ oauth2/passport.py
@@ -12,6 +12,17 @@ UUCIN_OAUTH2_DOMAIN = getattr(
 UUCIN_OAUTH2_TOKEN_CACHE = getattr(
     settings, 'UUCIN_OAUTH2_TOKEN_CACHE', ('uucin_oauth2_token', 60 * 5))
 
+UUCIN_OAUTH2_CLIENT_CACHE = getattr(
+    settings, 'UUCIN_OAUTH2_CLIENT_CACHE', ('uucin_oauth2_client', 60 * 5))
+
+
+def clean_client_data(uucode, client_id, access_token):
+    key = '%s:%s:%s' % (UUCIN_OAUTH2_CLIENT_CACHE[0], uucode, client_id)
+    old_token = cache.get(key)
+    if old_token:
+        cache.delete('%s:%s' % (UUCIN_OAUTH2_TOKEN_CACHE[0], old_token))
+    cache.set(key, access_token, UUCIN_OAUTH2_CLIENT_CACHE[1])
+
 
 def get_token_userid(access_token):
     if not access_token:
@@ -24,6 +35,9 @@ def get_token_userid(access_token):
         headers={'Authorization': 'token %s' % access_token})
     if resp.status_code == 200:
         result = json.loads(resp.content)
+
+        clean_client_data(result['userid'], result['client_id'], access_token)
+
         cache.set(
             '%s:%s' % (UUCIN_OAUTH2_TOKEN_CACHE[0], access_token),
             json.dumps(result, ensure_ascii=False),
